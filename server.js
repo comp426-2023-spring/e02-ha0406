3ea3f328ab48a75110d8a07667f2e0611fe63d01
root <root@UNC-PF2A5FYK.localdomain>
2023-05-07 01:06:03 -0400

    used almost raw a04 info to try to get server to connect properly

--- server.js
+++ server.js
@@ -77,13 +77,18 @@ app.use('/', express.static(staticpath))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// app endpoint
+app.get("/app/", (req, res) => {
+  res.status(200).send('200 OK');
+})
+
 // app rps and rpsls
 app.get("/app/rps/", (req, res) => {
-  res.status(200).send(JSON.stringify(rps(req.body.shot)));
+  res.status(200).send(JSON.stringify(rps()));
 })
 
 app.get("/app/rpsls/", (req, res) => {
-  res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+  res.status(200).send(JSON.stringify(rpsls()));
 })
 
 // /play/ rps and rpsls QUERY
@@ -111,6 +116,11 @@ app.get("/app/rps/play/:shot", (req, res) => {
 
 app.get("/app/rpsls/play/:shot", (req, res) => {
   res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
+
+// 404 status
+app.get("*", (req, res) => {
+  res.status(404).send("404 NOT FOUND");
 })
 
 // Create app listener
